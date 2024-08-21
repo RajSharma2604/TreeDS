4bd9c0e48aaa2cd53ca4adc9e26bd3a70a11fafc
RajSharma2604 <rajsharma240401@gmail.com>
2024-08-21 23:40:40 +0530

    binary tree creation

---
+++ binarytree/BTrep.cpp
@@ -0,0 +1,44 @@
+//
+#include<iostream>
+using namespace std;
+#include<bits/stdc++.h>
+class Node{
+    public:
+    int val;
+    Node *left;
+    Node *right;
+    Node(int data){
+        this->val=data;
+        this->left=NULL;
+        this->right=NULL;
+    }
+    
+};
+Node *buildtree(Node *root){
+    cout<<"enter data: "<<endl;
+    int data;
+    cin>>data;
+    root=new Node(data);
+    if(data==-1){
+        return NULL;
+    }
+    cout<<"enter the value to the left of "<<data<<" :";
+    root->left=buildtree(root->left);
+    cout<<"enter the value to the right of "<<data<<" :";
+    root->right=buildtree(root->right);
+    return root;
+    }
+void inorderT(Node *root){
+    if(root){
+        inorderT(root->left);
+        cout<<root->val<<" ";
+        inorderT(root->right);
+    }
+}
+int main()
+{   
+    Node *root=NULL;
+    root=buildtree(root);
+    inorderT(root);
+return 0;
+}
