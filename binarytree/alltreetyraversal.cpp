92afa3dd49930c01eeb2341e0d384aa35494db8c
RajSharma2604 <rajsharma240401@gmail.com>
2024-08-23 23:39:27 +0530

    all traversal

---
+++ binarytree/alltreetyraversal.cpp
@@ -0,0 +1,26 @@
+// // this code is going to be vector of vector ,andfirst we will get each vector of each traversal
+// // then pushed those traversal into vector and push all the vectors into a single vector
+
+
+// logic is allabout reference 
+
+
+// //treecreation is done.
+// #include <vector>;
+// void inorder(vector<int>&in,Node *root){
+//     samelogic
+// }
+
+// int main(){
+//     vector<int>in,pre,post;
+//     vector<vector<int>> ans;
+//     inorder(in,root);
+//     preorder(pre,root);
+//     postorder(post,root);
+//     ans.push_back(inorder);
+//     ans.push_back(preorder);
+//     ans.push_back(postorder);
+//     return ans;
+
+
+//     }
